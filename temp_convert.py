432bb08dc36e9005a27a651fe2ebceb083492d02
Wagner, J <35028309+dota247@users.noreply.github.com>
2021-04-05 08:55:17 -0700

    Add files via upload

---
+++ temp_convert.py
@@ -0,0 +1,16 @@
+# Author: Juan A Wagner
+# Date  : 4/01/2021
+# Description: The program takes in a temperature value, this value is converted into Fahrenheit.
+
+# records user input to be converted
+celsius = input("Please enter a celsius temperature:")
+
+# The users' input is type casted to float then plugged into [(9/5) * C + 32], where C is the user's input.
+fahrenheit = round((9/5) * float(celsius) + 32,1)
+
+# Returns results of conversion with a message
+print("The equivalent Fahrenheit temperature is: ")
+print(fahrenheit)
+
+
+
